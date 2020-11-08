04a89c5a466931b1f3bd8e474bd69327e5181ce8
gmanriquev <gmanrique.422@gmail.com>
2020-11-08 14:09:11 -0500

    Bloque 03

--- BLOQUE-03/Parte01.sql
+++ BLOQUE-03/Parte01.sql
@@ -24,3 +24,27 @@ inner join TipoDocumento t on c.codtipo=t.codtipo
 inner join Zona z on c.codzona=z.codzona
 where tipo='P' and c.estado=1
 order by [NOMBRE COMPLETO] asc
+
+--03.06
+
+select t.tipo as TIPO,t.numero as NUMERO,t.codcliente as COD_CLIENTE,
+c.razon_social as EMPRESA,z.nombre as ZONA
+from Telefono t 
+inner join Cliente c on t.codcliente=c.codcliente and c.tipo='E'
+inner join Zona z on c.codzona=z.codzona
+where t.estado=1
+
+--03.08
+--select isnull('MM','NN')
+
+select t.tipo as TIPO,t.numero as NUMERO,
+       case when c.tipo='E' then c.razon_social
+	        when c.tipo='P' then upper(concat(rtrim(ltrim(nombres)),' ',rtrim(ltrim(ape_paterno)),' ',rtrim(ltrim(ape_materno))))
+	   else 'SIN DETALLE'
+	   end as CLIENTE,
+	   isnull(email,'SIN DETALLE') as EMAIL,
+	   convert(varchar(8),getdate(),112) as FEC_CONSULTA --112:AAAAMMDD
+from Telefono t
+left join Cliente c on t.codcliente=c.codcliente
+where t.estado=1
+order by c.email desc
