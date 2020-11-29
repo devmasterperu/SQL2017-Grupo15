9c86e3b48fe774ad41a8586adbc322176e9acc72
gmanriquev <gmanrique.422@gmail.com>
2020-11-29 11:28:49 -0500

    Bloque 06 parte b

--- Parte02.sql
+++ Parte02.sql
@@ -95,3 +95,103 @@ as return
 
 select * from UF_Resumen_Cliente_Empresa()
 order by ZONA asc, TOTAL asc
+
+/*UNION y UNION ALL*/
+
+select 'A','B','C'
+union all
+select 'A','B','C'
+union all
+select 'A','B','D'
+
+select 'A','B','C'
+union all
+select '20','B',convert(varchar(8),getdate(),112)
+union all
+select 'A','B',null
+
+select 'A','B','C'
+union
+select 'A','B','C'
+union
+select 'A','B','D'
+
+select 'A','B'
+union
+select 'A','B'
+union 
+select 'A','B'
+
+--06.09
+--a.
+create view dbo.V_Ubigeo 
+as
+	with CTE_Ubigeos as
+	(
+	select cod_dpto,cod_prov,cod_dto from DEVWIFI15Ed.dbo.Ubigeo
+	union all
+	select cod_dpto,cod_prov,cod_dto from DevWifi2019.comercial.Ubigeo --DATABASE.SCHEMA.TABLE
+	)
+	select cod_dpto as CODIGO_DPTO,cod_prov as CODIGO_PROV,cod_dto as CODIGO_DTO
+	from CTE_Ubigeos
+
+select * from dbo.V_Ubigeo 
+order by CODIGO_DPTO,CODIGO_PROV,CODIGO_DTO
+
+--b.
+create function UF_Ubigeos() returns table as
+return
+	with CTE_Ubigeos as
+	(
+	select cod_dpto,cod_prov,cod_dto from DEVWIFI15Ed.dbo.Ubigeo
+	union
+	select cod_dpto,cod_prov,cod_dto from DevWifi2019.comercial.Ubigeo --DATABASE.SCHEMA.TABLE
+	)
+	select cod_dpto as CODIGO_DPTO,cod_prov as CODIGO_PROV,cod_dto as CODIGO_DTO
+	from CTE_Ubigeos
+
+select * from UF_Ubigeos() 
+order by CODIGO_DPTO,CODIGO_PROV,CODIGO_DTO
+
+--06.11
+select tipo,numero,codcliente,estado from DEVWIFI15Ed.dbo.Telefono 
+where tipo='WSP' and numero='963065267' and codcliente=3 and estado=1
+
+select tipo,numero,codcliente,estado from DevWifi2019.comercial.Telefono
+where tipo='WSP' and numero='963065267' and codcliente=3 and estado=1
+
+--a
+with CTE_Telefono as
+(
+select tipo,numero,codcliente,estado from DEVWIFI15Ed.dbo.Telefono      
+intersect
+select tipo,numero,codcliente,estado from DevWifi2019.comercial.Telefono 
+)
+select ct.tipo as TIPO,numero as NUMERO,ct.codcliente as CODIGO,ct.estado as ESTADO,
+coalesce(c.razon_social,c.nombres+' '+c.ape_paterno+' '+c.ape_materno,'SIN DATO') as CLIENTE,
+row_number() over(partition by ct.codcliente order by ct.numero asc) as POSICION
+from CTE_Telefono ct
+left join Cliente c on ct.codcliente=c.codcliente
+
+--b
+--SI (DBO) - NO (COMERCIAL)
+--select tipo,numero,codcliente,estado from DEVWIFI15Ed.dbo.Telefono      
+--except
+--select tipo,numero,codcliente,estado from DevWifi2019.comercial.Telefono 
+
+select tipo,numero,codcliente,estado from DevWifi2019.comercial.Telefono  
+where tipo='SMS' and numero='900670335' and codcliente=1 and estado=1
+select tipo,numero,codcliente,estado from DEVWIFI15Ed.dbo.Telefono 
+where tipo='SMS' and numero='900670335' and codcliente=1 and estado=1
+
+with CTE_Telefono as
+(--NO (DBO) - SI (COMERCIAL)
+select tipo,numero,codcliente,estado from DevWifi2019.comercial.Telefono    
+except
+select tipo,numero,codcliente,estado from DEVWIFI15Ed.dbo.Telefono   
+)
+select ct.tipo as TIPO,numero as NUMERO,ct.codcliente as CODIGO,ct.estado as ESTADO,
+coalesce(c.razon_social,c.nombres+' '+c.ape_paterno+' '+c.ape_materno,'SIN DATO') as CLIENTE,
+row_number() over(partition by ct.codcliente order by ct.numero asc) as POSICION
+from CTE_Telefono ct
+left join Cliente c on ct.codcliente=c.codcliente
